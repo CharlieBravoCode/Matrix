4abf86ebc40afcae0dfb668145e8924e7df9331c
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2023-05-08 17:08:05 +0200

    works

--- background.js
+++ background.js
@@ -1,22 +1,9 @@
-function setWindowSize(window) {
-  var screenWidth = screen.availWidth;
-  var screenHeight = screen.availHeight;
-
-  var width = Math.round(screenWidth * 0.8);
-  var height = Math.round(screenHeight * 0.8);
-
-  window.outerBounds.width = width;
-  window.outerBounds.height = height;
-}
-
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('matrix.html', {
-    'resizable': true,
     'outerBounds': {
-      'minWidth': 300,
-      'minHeight': 200
-    }
-  }, function(window) {
-    setWindowSize(window);
+      'width': 2560,
+      'height': 1440
+    },
+    'resizable': true
   });
 });
